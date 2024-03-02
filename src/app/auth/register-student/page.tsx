acf0b806bfc17c6a08841a6a53e4f1aa5dcf0470
Buiama <buialodima@i.ua>
2024-03-02 17:40:49 +0100

    feature: added registration pages

---
+++ src/app/auth/register-student/page.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const RegistrationSchema = z.object({
+  firstName: z.string().min(1, { message: "First name is required" }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+  email: z.string().min(1, { message: "Email is required" }),
+  group: z.string().min(1, { message: "Group is required" }),
+  course: z
+    .number()
+    .min(1, { message: "You need to mention your year of studying" }),
+  faculty: z.string().min(1, { message: "Faculty is required" }),
+  password: z.string().min(3, { message: "Password is required" }),
+  passwordConfirmation: z.string().min(3, { message: "Confirm your password" }),
+});
+
+const Page = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof RegistrationSchema>>({
+    resolver: zodResolver(RegistrationSchema),
+  });
+
+  const handleSubmit = () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-5 px-32 w-full"
+      >
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Create a student account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your details to create your student account.
+          </p>
+        </div>
+        <div className="flex flex-row space-x-6">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem className="w-7/12">
+                <FormLabel color="black">First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Dmytro" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel color="black">Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Buialo" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel color="black">Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="yourmail@example.com"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-row space-x-6">
+          <FormField
+            control={form.control}
+            name="faculty"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel color="black">Faculty</FormLabel>
+                <FormControl>
+                  <Input placeholder="FICE" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Abbreviated name of the faculty
+                </FormDescription>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="group"
+            render={({ field }) => (
+              <FormItem className="w-5/12">
+                <FormLabel color="black">Group</FormLabel>
+                <FormControl>
+                  <Input placeholder="IP-15" {...field} />
+                </FormControl>
+                <FormDescription>Academic group</FormDescription>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="course"
+            render={({ field }) => (
+              <FormItem className="w-5/12">
+                <FormLabel color="black">Year</FormLabel>
+                <FormControl>
+                  <Input placeholder="3" {...field} />
+                </FormControl>
+                <FormDescription>Year of studying</FormDescription>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel color="black">Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="••••••••••••" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="passwordConfirmation"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel color="black">Confirm password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="••••••••••••" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-between">
+          <Button type="submit">Submit</Button>
+          <Button
+            variant="ghost"
+            type="button"
+            onClick={() => router.push("./login")}
+          >
+            Login
+          </Button>
+        </div>
+        <div className="text-center text-xs">
+          You are a teacher? Register your account
+          <Button
+            variant="link"
+            size="sm"
+            type="button"
+            className="px-1 font-bold"
+            onClick={() => router.push("./register-teacher")}
+          >
+            here!
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default Page;
--- src/app/auth/register-teacher/page.tsx
+++ src/app/auth/register-teacher/page.tsx
@@ -9,19 +9,18 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
 const RegistrationSchema = z.object({
-  username: z.string().min(1, { message: "Username is required" }),
+  firstName: z.string().min(1, { message: "First name is required" }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
   email: z.string().min(1, { message: "Email is required" }),
-  password: z.string().min(1, { message: "Password is required" }),
-  passwordConfirmation: z.string().min(1, { message: "Confirm your password" }),
+  password: z.string().min(3, { message: "Password is required" }),
+  passwordConfirmation: z.string().min(3, { message: "Confirm your password" }),
 });
 
 const Page = () => {
@@ -36,33 +35,42 @@ const Page = () => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(handleSubmit)}
-        className="space-y-6 p-20"
+        className="space-y-5 px-32 w-full"
       >
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
-            Create an account
+            Create a teacher account
           </h1>
           <p className="text-sm text-muted-foreground">
-            Enter your details to create your account.
+            Enter your details to create your teacher account.
           </p>
         </div>
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel color="black">Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Hjyup" {...field} />
-              </FormControl>
-              <FormMessage />
-              <FormDescription>
-                Your username will be also displayed for a public. You can add
-                public name in the settings to change this behavior.
-              </FormDescription>
-            </FormItem>
-          )}
-        />
+        <div className="flex flex-row space-x-6">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem className="w-7/12">
+                <FormLabel color="black">First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Dmytro" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel color="black">Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Buialo" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
         <FormField
           control={form.control}
           name="email"
@@ -70,13 +78,8 @@ const Page = () => {
             <FormItem>
               <FormLabel color="black">Email</FormLabel>
               <FormControl>
-                <Input
-                  type="email"
-                  placeholder="yourmail@example.com"
-                  {...field}
-                />
+                <Input type="email" placeholder="yourmail@uni.com" {...field} />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
@@ -87,9 +90,8 @@ const Page = () => {
             <FormItem>
               <FormLabel color="black">Password</FormLabel>
               <FormControl>
-                <Input type="password" placeholder="••••••••••" {...field} />
+                <Input type="password" placeholder="••••••••••••" {...field} />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
@@ -100,9 +102,8 @@ const Page = () => {
             <FormItem>
               <FormLabel color="black">Confirm password</FormLabel>
               <FormControl>
-                <Input type="password" placeholder="••••••••••" {...field} />
+                <Input type="password" placeholder="••••••••••••" {...field} />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
@@ -114,6 +115,18 @@ const Page = () => {
             onClick={() => router.push("./login")}
           >
             Login
+          </Button>
+        </div>
+        <div className="text-center text-xs">
+          You are a student? Register your account
+          <Button
+            variant="link"
+            size="sm"
+            type="button"
+            className="px-1 font-bold"
+            onClick={() => router.push("./register-student")}
+          >
+            here!
           </Button>
         </div>
       </form>
