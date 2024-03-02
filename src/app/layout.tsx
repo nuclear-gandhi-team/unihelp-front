48fec46f6df1cd96c3d650b44a4c7bc5a7448ca4
danyilbutov <danyil.butov@solidgate.com>
2024-03-02 12:00:16 +0000

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "UniHelp - Help for students, by students",
+  description: "UniHelp is a platform for students to help each other out",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
