2f4b50241fc9504416e5621ab6cac83fa7deeca3
danyilbutov <danyil.butov@solidgate.com>
2024-03-02 12:17:17 +0000

    feat(dev): add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
