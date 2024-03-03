e2664ac79410de34242d92badff88243930b6202
HJK <89708817+HJyup@users.noreply.github.com>
2024-03-03 02:27:55 +0000

    feat: add blank page (#9)
    Co-authored-by: danyilbutov <danyil.butov@solidgate.com>

---
+++ src/lib/const/random-chart.ts
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    name: "Jan",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Feb",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Mar",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Apr",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "May",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Jun",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Jul",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Aug",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Sep",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Oct",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Nov",
+    total: Math.floor(Math.random() * 101),
+  },
+  {
+    name: "Dec",
+    total: Math.floor(Math.random() * 101),
+  },
+];
